cdbbdab18053c04529b0456ef0ff5e9b9fa75212
BALAJI POTHULA <balan.pothula@gmail.com>
2019-06-22 15:52:11 +0530

    Update setup-k8s-master.sh

--- setup-k8s-master.sh
+++ setup-k8s-master.sh
@@ -84,6 +84,3 @@ yum -y install kubelet kubeadm kubectl --disableexcludes=kubernetes
 systemctl  restart kubelet
 # run kubelet as daemon.
 systemctl enable --now kubelet
-
-# adding current user (non root user) to docker group.
-# sudo usermod -a -G docker $USER
