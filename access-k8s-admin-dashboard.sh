4b31db0d95295bcc124720b40f5f5e1efa235eaa
BALAJI POTHULA <balan.pothula@gmail.com>
2019-06-23 15:56:36 +0530

    Update access-k8s-admin-dashboard.sh

--- access-k8s-admin-dashboard.sh
+++ access-k8s-admin-dashboard.sh
@@ -1,5 +1,5 @@
 # 
-ssh -i "ec2.pem" -L 8001:127.0.0.1:8001 user@ip
+ssh -o ServerAliveInterval=60 -o ServerAliveCountMax=30 -o CheckHostIP=no -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i "vpc0-subnet0-ec2.pem" -L 8001:127.0.0.1:8001 ec2-user@13.233.9.48
 
 # open below url in your local browser.
 # http://localhost:8001/api/v1/namespaces/kube-system/services/https:kubernetes-dashboard:/proxy/#!/login
