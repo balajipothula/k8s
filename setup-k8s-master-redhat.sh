0d4e5f9ada5dd80da72760e3a589d8ac5a0a1df8
BALAJI POTHULA <balan.pothula@gmail.com>
2019-06-23 18:43:17 +0530

    Update setup-k8s-master-redhat.sh

--- setup-k8s-master-redhat.sh
+++ setup-k8s-master-redhat.sh
@@ -36,7 +36,12 @@ firewall-cmd --permanent --add-port=10250/tcp
 firewall-cmd --permanent --add-port=10251/tcp
 firewall-cmd --permanent --add-port=10252/tcp
 firewall-cmd --permanent --add-port=10255/tcp
+
 firewall-cmd --permanent --add-port=8001/tcp
+
+firewall-cmd --permanent --add-port=6783/tcp
+firewall-cmd --permanent --add-port=6783/udp
+firewall-cmd --permanent --add-port=6784/udp
 
 # reloading firewall.
 firewall-cmd --reload
