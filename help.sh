efb61baef2c00d65fb4f39475284e3fcfe996dff
BALAJI POTHULA <balan.pothula@gmail.com>
2019-06-26 14:05:25 +0530

    Update help.sh

--- help.sh
+++ help.sh
@@ -2,3 +2,6 @@
 sudo rm -rf /var/lib/weave/weave-netdata.db
 # removing weave pods.
 for i in $(kubectl get pods -n kube-system | awk '{print $1}' | grep weave); do kubectl delete pod -n kube-system $i; done
+
+# adding autocomplete permanently to bash shell.
+echo "source <(kubectl completion bash)" >> $HOME/.bashrc
