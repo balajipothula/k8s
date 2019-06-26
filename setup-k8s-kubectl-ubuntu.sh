93a2facb48c89bd6a920d2bb8ef9b86656b69de9
BALAJI POTHULA <balan.pothula@gmail.com>
2019-06-26 14:55:46 +0530

    Update setup-k8s-kubectl-ubuntu.sh

--- setup-k8s-kubectl-ubuntu.sh
+++ setup-k8s-kubectl-ubuntu.sh
@@ -4,19 +4,25 @@
 # Date        : 26 June 2019,
 # Description : Installing kubeclt on Ubuntu.
 
-# 
+# Note: Please run this script with sudo privilage.
+
+# updating packages index
+apt update
+
+# installing packages to allow apt to use repository over https.
+apt install -y apt-transport-https
+
+# adding kubernetes official gpg key.
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+
+# setting up the stable kubernetes repository.
+echo "deb https://apt.kubernetes.io/ kubernetes-$(lsb_release -cs) main" | tee -a /etc/apt/sources.list.d/kubernetes.list
+
+# updating packages index
+apt update
+
+# installing kubectl.
+apt install -y kubectl
 
 # 
-apt update && apt install -y apt-transport-https
-
-#
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-
-#
-echo "deb https://apt.kubernetes.io/ kubernetes-$(lsb_release --codename | cut -f2) main" | tee -a /etc/apt/sources.list.d/kubernetes.list
-
-#
-apt update && apt install -y kubectl
-
-#
 # multipass exec build-service -- /snap/bin/microk8s.config > $HOME/.kube/config
