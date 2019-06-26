76651f4a0227b2e272c416bd2941b7587dbdb1c3
BALAJI POTHULA <balan.pothula@gmail.com>
2019-06-26 15:26:14 +0530

    Update setup-k8s-weave-net-redhat.sh

--- setup-k8s-weave-net-redhat.sh
+++ setup-k8s-weave-net-redhat.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+# Author      : BALAJI POTHULA <balaji.pothula@techie.com>,
+# Date        : 20 June 2019,
+# Description : Kubernetes kube setup on RHEL7.
+
+# Please run this script with non root user (with out sudo)
+
+# exporting kubectl version
+export kubever=$(kubectl version | base64 | tr -d '\n')
+
+# setting up coredns(dns servers).
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$kubever"
