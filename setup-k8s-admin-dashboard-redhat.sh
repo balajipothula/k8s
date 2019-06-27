65edf5ec0c9f35efd904d6e2f557a92321329dfb
BALAJI POTHULA <balan.pothula@gmail.com>
2019-06-27 12:35:49 +0530

    Update setup-k8s-admin-dashboard-redhat.sh

--- setup-k8s-admin-dashboard-redhat.sh
+++ setup-k8s-admin-dashboard-redhat.sh
@@ -18,6 +18,9 @@ kubectl create serviceaccount dashboard -n default
 # adding cluster binding rules to dashboard account.
 kubectl create clusterrolebinding dashboard-admin -n default --clusterrole=cluster-admin --serviceaccount=default:dashboard
 
+# just new line.
+echo -e '\n'
+
 # getting secret token for dashboard.
 kubectl get secret $(kubectl get serviceaccount dashboard -o jsonpath="{.secrets[0].name}") -o jsonpath="{.data.token}" | base64 --decode
 
